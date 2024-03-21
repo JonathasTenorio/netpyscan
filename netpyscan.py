75ae07d879bddd4e2bbeaac5a74c774e4e6b0a28
Jonathas Gabriel Davide Tenorio <2009641@aluno.univesp.br>
2024-03-21 16:44:16 -0300

    Adicionado verificação de porta possívelmente filtrada

--- netpyscan.py
+++ netpyscan.py
@@ -16,26 +16,53 @@ def envia_pacote_icmp(target):
     return resposta_icmp
 
 def armazena_resposta_pacote_tcp(response):
+    resposta_string=str(response[TCP])
+    divide_resposta=resposta_string.split(">")
+    pega_flag_servico=divide_resposta[0].split(":")[-1].strip()
+    pega_resposta_servico=resposta_string.split()[-1]
+    hosts_encontrados[response[IP].src].setdefault('porta', {})
+    hosts_encontrados[response[IP].src]['porta'][response[TCP].sport] = {
+                                    'protocolo': 'TCP', 
+                                    'flag': pega_flag_servico, 
+                                    'estado': ' ', 
+                                    'resposta': pega_resposta_servico
+                                    }
+
+def armazena_resposta_pacote_tcperror(response):
+    hosts_encontrados[response[IP].src].setdefault('porta', {})
+    hosts_encontrados[response[IP].src]['porta'][response[TCPerror].dport] = {
+                                    'protocolo': 'TCP', 
+                                    'flag': '?',
+                                    'estado': ' ', 
+                                    }
+
+def analisa_resposta_pacote_tcp(response):
     if response and response.haslayer(TCP):
         if response[IP].src in hosts_encontrados:
-            resposta_string=str(response[TCP])
-            divide_resposta=resposta_string.split(">")
-            pega_flag_servico=divide_resposta[0].split(":")[-1].strip()
-            pega_resposta_servico=resposta_string.split()[-1]
-            hosts_encontrados[response[IP].src].setdefault('porta', {})
-            hosts_encontrados[response[IP].src]['porta'][response[TCP].sport] = {
-                                            'protocolo': 'TCP', 
-                                            'flag': pega_flag_servico, 
-                                            'estado': ' ', 
-                                            'resposta': pega_resposta_servico
-                                            }
+            armazena_resposta_pacote_tcp(response)
             if (response[TCP].flags == 18):
                 hosts_encontrados[response[IP].src]['porta'][response[TCP].sport]['estado'] = 'aberta'
-            else:
+            elif (response[TCP].flags == 20):
                 hosts_encontrados[response[IP].src]['porta'][response[TCP].sport]['estado'] = 'fechada'
+            else:
+                hosts_encontrados[response[IP].src]['porta'][response[TCP].sport]['estado'] = 'filtrada'
         else:
             hosts_encontrados[response[IP].src] = {'status': 'Filtrado'}
-       
+    elif response and response.haslayer(TCPerror):
+        armazena_resposta_pacote_tcperror(response)
+        hosts_encontrados[response[IP].src]['porta'][response[TCPerror].dport]['estado'] = 'filtrada'
+    else:
+        return 0
+
+def analiza_code(code,host,porta):
+    if code == 0:
+        hosts_encontrados[host].setdefault('porta', {})
+        hosts_encontrados[host]['porta'][porta] = {
+                                    'protocolo': 'TCP', 
+                                    'flag': '?',
+                                    'estado': 'filtrada', 
+                                    }
+
 def host_ativo(target):
     resposta_pacote_icmp=envia_pacote_icmp(target)
     if resposta_pacote_icmp and resposta_pacote_icmp.haslayer(ICMP):
@@ -55,10 +82,10 @@ def imprime_resultado_varredura(hosts_encontrados,array_argumentos):
             if (info['porta'] != ""):
                 for porta, info_porta in info['porta'].items():
                     if "-v" in array_argumentos:
-                        print(f'''[-]  {porta}/{info_porta["protocolo"]}  ({info_porta["flag"]})  [ {info_porta["estado"]} ]  {info_porta["resposta"]}''')
+                        print(f'''[-]  {porta}/{info_porta["protocolo"]}  ({info_porta["flag"]})  [ {info_porta["estado"]} ]  ''')
                     else:
-                        if(info_porta["estado"] == "aberta"):
-                            print(f'''[-]  {porta}/{info_porta["protocolo"]}  ({info_porta["flag"]})  [ {info_porta["estado"]} ]  {info_porta["resposta"]}''')
+                        if(info_porta["estado"] != "fechada"):
+                            print(f'''[-]  {porta}/{info_porta["protocolo"]}  ({info_porta["flag"]})  [ {info_porta["estado"]} ]  ''')
 
 def split_rede(target):
     aux=target.split(".")
@@ -74,7 +101,7 @@ def split_rede(target):
 def ajusta_endereco_alvo(verifica_enderecos):
     aux=verifica_enderecos.split(".")
     return aux
-    
+
 def define_alvo(target):
     rede=split_rede(target)
     endereco_ajustado=ajusta_endereco_alvo(target)
@@ -93,22 +120,25 @@ def porta_em_porta(host,ports,ifTrue):
     if ifTrue:        
         for porta in range(int(ports[0]), int(ports[-1])+1):
             resposta_pacote=envia_pacote_tcp(host, porta)
-            armazena_resposta_pacote_tcp(resposta_pacote)
+            code=analisa_resposta_pacote_tcp(resposta_pacote)
+            analiza_code(code,host,porta)
     else:
         for porta in ports:
             resposta_pacote=envia_pacote_tcp(host, porta)
-            armazena_resposta_pacote_tcp(resposta_pacote)
-        
+            code=analisa_resposta_pacote_tcp(resposta_pacote)
+            analiza_code(code,host,porta)
+
 def host_em_hosts(hosts_encontrados,port):
     for host in hosts_encontrados:
         resposta_pacote=envia_pacote_tcp(host, port)
-        armazena_resposta_pacote_tcp(resposta_pacote)
+        code=analisa_resposta_pacote_tcp(resposta_pacote)
+        analiza_code(code,host,port)
 
 def argumento_erre(hosts_encontrados,ports,ifTrue):
     if ifTrue:        
         for porta in range(int(ports[0]), int(ports[-1])+1):
             host_em_hosts(hosts_encontrados,porta)
-            
+
     else:
         for porta in ports:
             host_em_hosts(hosts_encontrados,porta)
@@ -127,11 +157,10 @@ def ajusta_argumento_dois(ports):
 
     return portas,eTrue
 
-
 def main(argumentos):
 
     configuracao_verbosa()
-    
+
     target = argumentos[1]
 
     define_alvo(target)
@@ -148,20 +177,6 @@ def main(argumentos):
 
     imprime_resultado_varredura(hosts_encontrados,array_argumentos=argumentos)
 
-
-
 if __name__ == '__main__':
     argumentos=sys.argv
     main(argumentos)
-
-
-# falta passar verificação de quantidade de hosts separados por - ou , ou por /bits (feito)
-# falta verificação de formato do ip passado (cancelado)
-# falta range de portas e verificar se é range ou valores específicos  (feito)
-# falta verificação de modo verboso -v para exibir portas fechadas (feito)
-# falta verificação do -f para verificar se o host esta filtrando o protocolo icmp em caso de não haver resposta  (feito)
-# falta verificar quantos métodos argumentos foram passados para que todos sejam chamados da devida forma  (feito)
-# falta criar uma main function para que esta organize e ordene os códigos
-# falta criar função para verificar a resposta dos hosts
-# falta criar função que armazene sómente as respostas e não deixar tudo junto da função que realiza o envio do pacote SYN  (feito)
-# falta criar argumento -o para salvar a saída em um arquivo .log e se for -v -o salvar a saída com as portas fechadas também 
